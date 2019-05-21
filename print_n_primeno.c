5e1b0c3b290b1c34fc47e07f00374e494a94f933
Selvabharathi S <selvabharathi327@gmail.com>
2019-05-21 16:42:03 +0530

    Update print_n_primeno.c

--- print_n_primeno.c
+++ print_n_primeno.c
@@ -5,16 +5,16 @@ int main()
   scanf("%d",&num);
   for(int i=1;i<=num;i++)
   {
-    int flag=1;
-    for(int j=2;j<=i-1;j++)
+    int c=0;
+    for(int j=2;j<=i/2;j++)
     {
       if(i%j==0)
       {
-        flag=0;
+        c++;
         break;
       }
     }
-    if(flag==1)
+    if(c==0 && c!=1)
       printf("%d ",i);
   }
   return 0;
